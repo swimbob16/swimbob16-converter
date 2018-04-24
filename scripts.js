95e0f9aacf1b2d12a5f4c33210ee700115e9b709
Devin Luensman <devin.luensman@student.swic.edu>
2018-04-23 23:28:50 -0500

    Re-finished homework. Had conversion backwards.

--- scripts.js
+++ scripts.js
@@ -6,9 +6,9 @@ const form = document.querySelector("form");
  * @param {Number} userInput
  * @returns {Number} - return the converted measurement
  */
-function dynesToNewtons(userInput) {
+function newtonsToDynes(userInput) {
   /* TODO: Update the name of this function to something appropriate (e.g. feetToInches...) */
-  return Math.pow(userInput, 5);
+  return userInput * Math.pow(10, 5);
 }
 
 /** Capture the user's input value on submit and run it through the convert fxn.
@@ -18,13 +18,13 @@ function submitHandler() {
   const userInput = document.querySelector("#user-input").value;
 
   // Convert input value using converter function
-  const userOutput = dynesToNewtons(userInput);
+  const userOutput = newtonsToDynes(userInput);
 
   // Grab the p html tag
   const p = document.querySelector("#user-output");
 
   // Output conversion statement to p tag
-  p.textContent = `${userInput.toLocaleString()} dyne(s) is ${userOutput.toLocaleString()} Newton(s)!`;
+  p.textContent = `${userInput.toLocaleString()} newtons is equal to ${userOutput.toLocaleString()} dynes!`;
 }
 
 // Add a listener to the form
